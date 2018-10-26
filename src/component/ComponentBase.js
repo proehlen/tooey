3be6179f7c87c4bf2f63292a1d5b7566e3ce62de
Peter Roehlen <peter.roehlen@gmail.com>
2018-10-26 07:51:38 +0700

    Document ComponentBase and Form

--- src/component/ComponentBase.js
+++ src/component/ComponentBase.js
@@ -1,22 +1,38 @@
+/* eslint-disable no-unused-vars */
 // @flow
+
+/**
+ * The base class for all renderable and interactive components.
+ *
+ * There are two methods that must be present in all components:
+ * `render` and `handle`.  These methods and the expecteded interface
+ * are provided by this base but they must be overriden in child
+ * classes.
+ */
 export default class ComponentBase {
   /**
    * Synchronous function to render the component.
+   * @abstract
+   *
+   * The inactive parameter is to be used where a component is to
+   * be rendered but is not accepting input and while inactive
+   * has a different visual appearance.  For example, a menu
+   * that is inactive while a user's input is being directed to
+   * an input field may not have bolded short cut keys.
    */
-  // eslint-disable-next-line no-unused-vars
   render(inactive: boolean) {
     throw new Error('Method is abstract.  Override in subclass.');
   }
 
 
   /**
-   * Async function to handle user input
+   * Asynchronous function to handle user input
+   * @abstract
    *
    * Returns promise that resolves to boolean where true
    * means this component handled the input and no other
    * component should respond to it.
    */
-  // eslint-disable-next-line no-unused-vars
   async handle(key: string): Promise<boolean> {
     throw new Error('Method is abstract.  Override in subclass.');
   }
